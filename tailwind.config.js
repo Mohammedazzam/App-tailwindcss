255cc79b83795bdd0b1cd6f6f0940530420b7397
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-06-11 14:33:23 +0300

    (15 تجهيز الهيدر)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
                 "maven": ["Maven Pro", "sans-serif  "]
             },
             colors: {
-                textColor: "#5b5b5b"
+                textColor: "#5b5b5b",
+                grey: "#EEEEEE",
+                secondryColor: "#47465A"
             }
         },
     },
