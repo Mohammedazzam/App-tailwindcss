e8fec2470711b749711281de64f3257dc9372d61
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-06-14 12:09:07 +0300

    (23+24 section contact)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        "maven": ["Maven Pro","sans-serif"]
-      },
-      colors: {
-        textColor: "#5b5b5b",
-        grey: "#EEEEEE",
-        secondryColor: "#47465A",
-        headingColor: "#353535",
-        headingColor2: "#484848"
-      }
+    theme: {
+        extend: {
+            fontFamily: {
+                "maven": ["Maven Pro", "sans-serif"]
+            },
+            colors: {
+                textColor: "#5b5b5b",
+                grey: "#EEEEEE",
+                secondryColor: "#47465A",
+                headingColor: "#353535",
+                headingColor2: "#484848"
+            },
+            spacing: {
+                "500": "31.25rem"
+            },
+            width: {
+                "4/7": "45%"
+            }
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {},
+    plugins: [],
 }
